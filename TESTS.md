966e994137630921bd224ef6afaf69f108fc4c24
Erik Frèrejean <erikfrerejean@phpbb.com>
2011-01-25 16:38:03 +0100

    [feature/unit-tests] Add PHPUnit support

---
+++ TESTS.md
@@ -0,0 +1,14 @@
+[![Support Toolkit](http://www.phpbb.com/theme/images/support/support_toolkit_logo_small.png)](http://www.phpbb.com/support/stk/ "Support Toolkit")
+
+## PHPUnit
+The Support Toolkit unit tests use PHPUnit framework. Version 3.5 or 
+better is required to run the tests. PHPUnit prefers to be installed
+via PEAR; refer to http://www.phpunit.de/ for more information.
+
+## Running
+Once the prerequisites are installed, run the tests from the project root directory (above `stk/`):
+	$ phpunit
+
+## More Information
+Further information is available on phpBB wiki:
+http://wiki.phpbb.com/display/DEV/Unit+Tests
---
+++ tests/stk_test_case.php
@@ -0,0 +1,50 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2008 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+class stk_test_case extends PHPUnit_Framework_TestCase
+{
+	protected $test_case_helpers;
+
+	public function __construct($name = NULL, array $data = array(), $dataName = '')
+	{
+		parent::__construct($name, $data, $dataName);
+		$this->backupStaticAttributesBlacklist += array(
+			'PHP_CodeCoverage' => array('instance'),
+			'PHP_CodeCoverage_Filter' => array('instance'),
+			'PHP_CodeCoverage_Util' => array('ignoredLines', 'templateMethods'),
+			'PHP_Timer' => array('startTimes',),
+			'PHP_Token_Stream' => array('customTokens'),
+			'PHP_Token_Stream_CachingFactory' => array('cache'),
+		);
+	}
+
+	public function setExpectedTriggerError($errno, $message = '')
+	{
+		$exceptionName = '';
+		switch ($errno)
+		{
+			case E_NOTICE:
+			case E_STRICT:
+				PHPUnit_Framework_Error_Notice::$enabled = true;
+				$exceptionName = 'PHPUnit_Framework_Error_Notice';
+			break;
+
+			case E_WARNING:
+				PHPUnit_Framework_Error_Warning::$enabled = true;
+				$exceptionName = 'PHPUnit_Framework_Error_Warning';
+			break;
+
+			default:
+				$exceptionName = 'PHPUnit_Framework_Error';
+			break;
+		}
+		$this->expectedTriggerError = true;
+		$this->setExpectedException($exceptionName, (string) $message, $errno);
+	}
+}
