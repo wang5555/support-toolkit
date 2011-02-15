fd7b28552c2e1bfba01fcb060fcad5db3ffda4b0
Erik Frèrejean <erikfrerejean@phpbb.com>
2011-02-15 13:35:28 +0100

    [feature/stand-alone] Move the copying into a function

--- build/update_vendor.sh
+++ build/update_vendor.sh
@@ -11,10 +11,28 @@ files=(
 
 # List with phpBB directories, if you need a complete
 # directory you can add them to this list instead of
-# having to add all the files seperate
+# having to add all the files separate
 dirs=(
 
 )
+
+# Function that handles the copy
+copyLib()
+{
+	if [ $1 == "umil" ]; then
+		echo "Copying the UMIL core"
+		cp ./vendor/UMIL/umil/root/umil/umil.php ./stk/lib/UMIL/umil.php
+	else
+		echo "Copying $1"
+		dn=`dirname $1`
+
+		if [ ! -d "./stk/lib/phpBB/$dn" ]; then
+			mkdir -p "./stk/lib/phpBB/$dn"
+		fi
+
+		cp -r "./vendor/phpBB/phpBB/$1" "./stk/lib/phpBB/$1"
+	fi
+}
 
 # Make sure that the vendor submodules are updated
 cd ..
@@ -31,29 +49,14 @@ fi
 # Copy over the phpBB files
 for f in ${files[@]}
 do
-	echo "Copying $f"
-	dn=`dirname $f`
-
-	if [ ! -d "./stk/lib/phpBB/$dn" ]; then
-		mkdir -p "./stk/lib/phpBB/$dn"
-	fi
-
-	cp "./vendor/phpBB/phpBB/$f" "./stk/lib/phpBB/$f"
+	copyLib "$f"
 done
 
 # Copy over the phpBB directories
 for d in ${dirs[@]}
 do
-	echo "Copying $d"
-	dn=`dirname $d`
-
-	if [ ! -d "./stk/lib/phpBB/$dn" ]; then
-		mkdir -p "./stk/lib/stk/$dn"
-	fi
-
-	cp -r "./vendor/phpBB/phpBB/$d" "./stk/lib/phpBB/$d"
+	copyLib "$d"
 done
 
 # Copy over the UMIL core
-echo "Copying the UMIL core"
-cp ./vendor/UMIL/umil/root/umil/umil.php ./stk/lib/UMIL/umil.php
+copyLib "umil"
